1cf60ffab620103c32cb57d8ca7813215d412d60
Vasco Meerman <vazz_m@hotmail.com>
2020-11-17 16:52:48 +0100

    Re-factored test and ingredient viewsets

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,45 +5,38 @@ from recipe import serializers
 from core.models import Tag, Ingredient
 
 
-class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
-    """Manage ingredients in the database"""
-
+class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
+                            mixins.ListModelMixin,
+                            mixins.CreateModelMixin):
+    """Base viewset for user owned recipe attributes"""
     # Our authentication methods
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+
+    # This overridden method will be called when the viewset wants the model instances,
+    # for this viewset. So we want to filter for only the current authenticated user
+    # And this also orders the instance in reverse aplhabetical order
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    # Override this to add the foreign keyed user to the tag
+    def perform_create(self, serializer):
+        """Create a new model"""
+        serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """Manage ingredients in the database"""
 
     # Objects to use for this viewset
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
 
-    # This overridden method will be called when the viewset wants the model instances,
-    # for this viewset. So we want to filter for only the current authenticated user
-    # And this also orders the instance in reverse aplhabetical order
-    def get_queryset(self):
-        """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
 
-    # Override this to add the foreign keyed user to the tag
-    def perform_create(self, serializer):
-        """Create a new ingredient"""
-        serializer.save(user=self.request.user)
-
-
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+class TagViewSet(BaseRecipeAttrViewSet):
     """Manage tags in the database"""
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+
+    # Objects to use for this viewset
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
-
-    # This overridden method will be called when the viewset wants the model instances,
-    # for this viewset. So we want to filter for only the current authenticated user
-    # And this also orders the instance in reverse aplhabetical order
-    def get_queryset(self):
-        """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    # Override this to add the foreign keyed user to the tag
-    def perform_create(self, serializer):
-        """Create a new tag"""
-        serializer.save(user=self.request.user)
